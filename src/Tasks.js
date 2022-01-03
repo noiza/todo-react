8374e4a21a5fa11e461c06a562bece70970ebbf7
Nataliia <dblack0000@gmail.com>
2022-01-03 20:26:44 +0100

    Added functionality to the new-task input

--- src/Tasks.js
+++ src/Tasks.js
@@ -6,15 +6,24 @@ import axios from "axios";
 
 const Tasks = () => {
   const [tasks, setTasks] = useState(null);
-  const [showCompletedTasks, setShowCompletedTasks] = useState(false);
-  const [showTodayTasks, setShowTodayTasks] = useState(false);
+  const [showCompletedTasks, setShowCompletedTasks] = useState(false); // todo: to store a showCompletedTasks value on the server
+  const [showTodayTasks, setShowTodayTasks] = useState(false); // todo: to store a showTodayTasks value on the server
   const [taskQuantity, setTaskQuantity] = useState(0);
   const [popperIsOpen, setPopperIsOpen] = useState(false);
+  const [newTaskTitle, setNewTaskTitle] = useState('');
+
+  const getTodayDate = () => {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+    return today = mm + '/' + dd + '/' + yyyy;    
+  };
+  const todayDate = getTodayDate();
 
   useEffect(() => {
     axios.get("http://localhost:3001/tasks/").then(({ data }) => {
       let tempTasks = data; // or // = [...data];
-      let todayDate = getTodayDate();
 
       if (!showCompletedTasks) {
         tempTasks = tempTasks.filter(task => task.completed === false);
@@ -29,15 +38,20 @@ const Tasks = () => {
     });
   }, [showCompletedTasks, showTodayTasks]);
 
-  const addTask = () => {
-    const title = window.prompt("New Task:");
-    if (title) {
-      const id = Math.floor(Math.random() * 1000) + 1;
-      const newTask = { id, title, completed: false };
-      setTasks([...tasks, newTask]);
-      axios.post("http://localhost:3001/tasks/", newTask).catch(() => {
-        alert("Error while adding the task.");
-      });
+  // onKeyDownHandler adds a new task by Enter
+  const onKeyDownHandler = (e) => {
+    if (e.key === "Enter") {
+
+      if (newTaskTitle) {
+        const id = Math.floor(Math.random() * 1000) + 1;
+        const newTask = { id, title: newTaskTitle, date: todayDate, completed: false };
+        setTasks([...tasks, newTask]);
+        setNewTaskTitle('');
+
+        axios.post("http://localhost:3001/tasks/", newTask).catch(() => {
+          alert("Error while adding the task.");
+        });
+      }
     }
   };
 
@@ -88,14 +102,6 @@ const Tasks = () => {
     }
   };
 
-  const getTodayDate = () => {
-    let today = new Date();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    let yyyy = today.getFullYear();
-    return today = mm + '/' + dd + '/' + yyyy;    
-  };
-
   return (
     <>
       <div className="header">
@@ -103,8 +109,6 @@ const Tasks = () => {
           <h2>to do:</h2>
 
           <div className="popper-wrap">
-            {/* todo: addTask */}
-            {/* <button onClick={() => addTask()}>+</button> */}
             <button className="popper-btn" onClick={() => setPopperIsOpen(true)}>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path d="M15 14.5a2 2 0 011.936 1.498L19.5 16a.5.5 0 010 1l-2.563.001a2.001 2.001 0 01-3.874 0L4.5 17a.5.5 0 010-1l8.564-.002A2 2 0 0115 14.5zm-.982 1.81l.005-.025-.005.026-.003.014-.004.025-.007.061A.897.897 0 0014 16.5l.008.125.007.047-.001.002.003.014.006.024h-.001l.004.018.016.058.007.021.004.013.009.026.013.033.012.027-.011-.026.019.043-.008-.017.029.06-.018-.037.048.09a1 1 0 001.784-.155l.015-.039.006-.018-.015.039.022-.06-.001-.001.016-.057.004-.018.005-.024.001-.006v-.001l.005-.033.008-.06A.877.877 0 0016 16.5l-.008-.124-.007-.051-.001-.001-.003-.014-.01-.047-.004-.016-.007-.024-.01-.034-.004-.012-.01-.03-.006-.013-.007-.017-.01-.026a.998.998 0 00-1.843.043l-.014.034-.007.022-.014.047-.002.009v.001l-.005.016-.01.047zM9 9.5a2 2 0 011.936 1.498L19.5 11a.5.5 0 010 1l-8.563.001a2.001 2.001 0 01-3.874 0L4.5 12a.5.5 0 010-1l2.564-.002A2 2 0 019 9.5zm0 1a.998.998 0 00-.93.634l-.014.034-.007.022-.014.047-.002.009v.001l-.005.016-.01.047.005-.025-.005.026-.003.014-.004.025-.007.061C8 11.441 8 11.471 8 11.5l.008.125.007.047-.001.002.003.014.006.024h-.001l.004.018.016.058.007.021.004.013.009.026.013.033.012.027-.011-.026.019.043-.008-.017.029.06-.018-.037.048.09a1 1 0 001.784-.155l.015-.039.006-.018-.015.039.022-.06-.001-.001.016-.057.004-.018.005-.024.001-.006v-.001l.005-.033.008-.06A.877.877 0 0010 11.5l-.008-.124-.007-.051-.001-.001-.003-.014-.01-.047-.004-.016-.007-.024-.01-.034-.004-.012-.01-.03-.006-.013-.007-.017-.01-.026A1.002 1.002 0 009 10.5zm6-6a2 2 0 011.936 1.498L19.5 6a.5.5 0 010 1l-2.563.001a2.001 2.001 0 01-3.874 0L4.5 7a.5.5 0 010-1l8.564-.002A2 2 0 0115 4.5zm0 1a.998.998 0 00-.93.634l-.014.034-.007.022-.014.047-.002.009v.001l-.005.016-.01.047.005-.025-.005.026-.003.014-.004.025-.007.061C14 6.441 14 6.471 14 6.5l.008.125.007.047-.001.002.003.014.006.024h-.001l.004.018.016.058.007.021.004.013.009.026.013.033.012.027-.011-.026.019.043-.008-.017.029.06-.018-.037.048.09a1 1 0 001.784-.155l.015-.039.006-.018-.015.039.022-.06-.001-.001.016-.057.004-.018.005-.024.001-.006v-.001l.005-.033.008-.06C16 6.557 16 6.528 16 6.5l-.008-.124-.007-.051-.001-.001-.003-.014-.01-.047-.004-.016-.007-.024-.01-.034-.004-.012-.01-.03-.006-.013-.007-.017-.01-.026A1.002 1.002 0 0015 5.5z" fill="currentColor"></path>
@@ -144,8 +148,15 @@ const Tasks = () => {
 
           </div>
         </div>
-        
-        <input className="new-task" type="text" placeholder="Add a task..."/>
+
+        <input 
+          className="new-task"
+          type="text"
+          placeholder="Add a task..."
+          value={newTaskTitle}
+          onChange={(e) => setNewTaskTitle(e.target.value)}
+          onKeyDown={onKeyDownHandler}
+        />
 
         <h3>{showTodayTasks ? "Today" : "All"} ({taskQuantity}):</h3>
       </div>
