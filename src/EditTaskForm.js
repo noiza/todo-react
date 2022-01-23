8ac0603145528b91e9387e6b9723fd02e2884a93
Nataliia <dblack0000@gmail.com>
2022-01-23 21:15:38 +0100

    Added focus to input on EditTaskForm

--- src/EditTaskForm.js
+++ src/EditTaskForm.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const EditTaskForm = ({task, onEdit, setIsEditFormOpen}) => {
   const [newTitle, setNewTitle] = useState(task.title);
+  const inputRef = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -11,6 +12,10 @@ const EditTaskForm = ({task, onEdit, setIsEditFormOpen}) => {
     }
   };
 
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return(
     <div className="task bg-edit">
       <form className="task-edit-form" onSubmit={handleSubmit}>
@@ -19,6 +24,8 @@ const EditTaskForm = ({task, onEdit, setIsEditFormOpen}) => {
           type="text"
           value={newTitle}
           onChange={(e) => setNewTitle(e.target.value)}
+          ref={inputRef}
+          onFocus={(e) => e.target.select()}
         />
         
         <div className="task-edit-actions">
