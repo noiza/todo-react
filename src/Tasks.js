c587276bbc59133f239e49a522dfb31af340af4a
Nataliia <dblack0000@gmail.com>
2022-01-28 16:31:37 +0100

    Added a custom placeholder for react-beautiful-dnd

--- src/Tasks.js
+++ src/Tasks.js
@@ -5,6 +5,17 @@ import Task from "./Task";
 import ArchivedTask from "./ArchivedTask";
 import EditTaskForm from "./EditTaskForm";
 
+
+const getItemStyle = (isDragging, draggableStyle) => ({  
+  // change background color if dragging
+  background: isDragging ? "#f5f6f8" : "white",
+    
+  // styles we need to apply on draggables
+	...draggableStyle
+});
+
+const queryAttr = "data-rbd-drag-handle-draggable-id";
+
 const Tasks = () => {
   const [tasks, setTasks] = useState(null);
   const [showArchivedTasks, setShowArchivedTasks] = useState(true);
@@ -15,6 +26,8 @@ const Tasks = () => {
 
   const [editTaskId, setEditTaskId] = useState(null);
   const [isEditFormOpen, setIsEditFormOpen] = useState(false);
+  
+  const [placeholderProps, setPlaceholderProps] = useState({});
 
   const getTodayDate = () => {
     let today = new Date();
@@ -116,6 +129,46 @@ const Tasks = () => {
         alert("Error while archiving the task.");
       });
   };
+  
+	const onDragEnd = result => {
+		// dropped outside the list
+		if (!result.destination) {
+			return;
+		}
+
+    setPlaceholderProps({});
+	};
+  
+  const onDragUpdate = update => {
+    if(!update.destination){
+      return;
+    }
+		const draggableId = update.draggableId;
+		const destinationIndex = update.destination.index;
+
+		const domQuery = `[${queryAttr}='${draggableId}']`;
+		const draggedDOM = document.querySelector(domQuery);
+
+		if (!draggedDOM) {
+			return;
+		}
+		const { clientHeight, clientWidth } = draggedDOM;
+
+		const clientY = parseFloat(window.getComputedStyle(draggedDOM.parentNode).paddingTop) + [...draggedDOM.parentNode.children]
+			.slice(0, destinationIndex)
+			.reduce((total, curr) => {
+				const style = curr.currentStyle || window.getComputedStyle(curr);
+				const marginBottom = parseFloat(style.marginBottom);
+				return total + curr.clientHeight + marginBottom;
+			}, 0);
+
+		setPlaceholderProps({
+			clientHeight,
+			clientWidth,
+      clientY,
+      clientX: parseFloat(window.getComputedStyle(draggedDOM.parentNode).paddingLeft)
+		});
+	};
 
   var archivedTasks = tasks && tasks.filter(x => x.archive);
   var activeTasks = tasks && tasks
@@ -202,18 +255,25 @@ const Tasks = () => {
         <h3>{showTodayTasks ? "Today" : "All"} ({activeTasks && activeTasks.length || 0}):</h3>
       </div>
 
-      <DragDropContext>
+      <DragDropContext onDragEnd={onDragEnd} onDragUpdate={onDragUpdate}>
         <Droppable droppableId="tasks">
           {(provided) => (
-            <div className="tasks task-list" {...provided.droppableProps} ref={provided.innerRef}>
+            <div className="tasks task-list"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
               {activeTasks && activeTasks.length > 0 ? (
                 activeTasks.map((task, index) => (
                   <Draggable key={task.id} draggableId={task.title} index={index}>
-                    {(provided) => (
+                    {(provided, snapshot) => (
                       <div 
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
                         ref={provided.innerRef}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}
                       >
                         {isEditFormOpen && task.id === editTaskId ?
                           <EditTaskForm
@@ -240,6 +300,15 @@ const Tasks = () => {
                 </div>
               )}
               {provided.placeholder}
+              <div style={{
+                position: "absolute",
+                top: placeholderProps.clientY,
+                left: placeholderProps.clientX,
+                height: placeholderProps.clientHeight,
+                borderRadius: 3,
+                border: "1px dashed lightgrey",
+                width: placeholderProps.clientWidth
+              }}></div>
             </div>
           )}
         </Droppable>
